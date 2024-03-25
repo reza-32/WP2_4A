b90a8cf35a26d39cc5dc763bad0e3c346a90d637
reza-32 <reza.maulana56@gmail.com>
2024-03-25 09:56:53 +0700

    tugas 1

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,8 +4,8 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Reza Maulana
-            Saya tinggal di Kota Pontianak
+        echo"Nama saya Reza Maulana,
+            Saya tinggal di Kota Pontianak,
             olah raga yang saya sukai adalah
             Running";
     }
